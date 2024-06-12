1930ab406526a4fe7671ffe4c6ad7e9512e6a672
Samuel Bamert <samuel.bamert@helbling.ch>
2024-06-12 13:39:09 +0200

    launch file for rplidar

---
+++ helmoro_base/helmoro_real/helmoro_real_bringup/launch/lidar_launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Directories
+    pkg_rplidar_ros = get_package_share_directory('rplidar_ros')
+
+    # Paths
+    rplidar_launch = PathJoinSubstitution([pkg_rplidar_ros, 'launch', 'view_rplidar_a2m8_launch.py'])
+
+    # ROS2 RPLidar A2M8 Node
+    rplidar_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([rplidar_launch])
+    )
+
+    # Create launch description and add actions
+    ld = LaunchDescription()
+    ld.add_action(rplidar_node)
+    return ld
