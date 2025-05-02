3a785e82898392d27968f63b05bce32ba87fa043
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-02 13:16:05 +0000

    fix(entrypoint): prevent crash with set +x

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -e
+set +e
 
 # Source environment variables
 source /opt/ros/jazzy/setup.bash
 source /home/ws/install/setup.bash
-source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+#source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 
 # Execute the provided command
 exec "$@"
