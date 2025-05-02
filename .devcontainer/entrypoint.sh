1f894c222b6c8a67ee302afe4854bb3acac39736
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-02 14:29:17 +0000

    fix(workflows): specified default work directory

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,7 +4,7 @@ set +e
 # Source environment variables
 source /opt/ros/jazzy/setup.bash
 source /home/ws/install/setup.bash
-#source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 
 # Execute the provided command
 exec "$@"
