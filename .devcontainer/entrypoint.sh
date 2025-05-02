ec09fbb76db1b7a060ca11647c9add7befae9e15
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-02 15:14:49 +0000

    fix(container): moved env variables to dockerfile,  sourcing to entrypoint.sh and simplified build context"

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set +e
+
+# Source environment variables
+source /opt/ros/jazzy/setup.bash
+source /home/ws/install/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+
+# Execute the provided command
+exec "$@"
