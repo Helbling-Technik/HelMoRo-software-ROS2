b8dc85e0fb27a9dde130813ac91fdf0b57af5e4a
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-12 07:33:12 +0000

    feat(description): apply black-formatter

--- src/helmoro_description/launch/description.launch.py
+++ src/helmoro_description/launch/description.launch.py
@@ -10,86 +10,115 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 ARGUMENTS = [
-    DeclareLaunchArgument('run_in_simulation', default_value='false',
-                          choices=['true', 'false'],
-                          description='run_in_simulation'),
-    DeclareLaunchArgument('robot_name', default_value='id_23',
-                          description='Robot name'),
-    DeclareLaunchArgument('namespace', default_value=LaunchConfiguration('robot_name'),
-                          description='Robot namespace'),
+    DeclareLaunchArgument(
+        "run_in_simulation",
+        default_value="false",
+        choices=["true", "false"],
+        description="run_in_simulation",
+    ),
+    DeclareLaunchArgument(
+        "namespace",
+        default_value="empty_namespace",
+        description="Robot namespace",
+    ),
 ]
 
 
 def generate_launch_description():
-    pkg_helmoro_description = get_package_share_directory('helmoro_description')
-    xacro_file = PathJoinSubstitution([pkg_helmoro_description,
-                                       'urdf',
-                                       'helmoro.urdf.xacro'])
+    pkg_helmoro_description = get_package_share_directory("helmoro_description")
+    xacro_file = PathJoinSubstitution(
+        [pkg_helmoro_description, "urdf", "helmoro.urdf.xacro"]
+    )
 
     robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        namespace=LaunchConfiguration('namespace'),
-        output='screen',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace=LaunchConfiguration("namespace"),
+        output="screen",
         parameters=[
-            {'use_sim_time': LaunchConfiguration('run_in_simulation')},
-            {'robot_description': ParameterValue(
-                Command([
-                    'xacro', ' ', xacro_file, ' ',
-                    'run_in_simulation:=', LaunchConfiguration('run_in_simulation'), ' ',
-                    'namespace:=', LaunchConfiguration('namespace')
-                ]), value_type=str)},
+            {"use_sim_time": LaunchConfiguration("run_in_simulation")},
+            {
+                "robot_description": ParameterValue(
+                    Command(
+                        [
+                            "xacro",
+                            " ",
+                            xacro_file,
+                            " ",
+                            "run_in_simulation:=",
+                            LaunchConfiguration("run_in_simulation"),
+                            " ",
+                            "namespace:=",
+                            LaunchConfiguration("namespace"),
+                        ]
+                    ),
+                    value_type=str,
+                )
+            },
         ],
-        remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
     )
-    
-    # Workaround until Gazebo Ionic upgrades to sdformat 1.12 and allows <frame_id> tags
-    static_transforms_publisher = GroupAction([
-        PushRosNamespace(LaunchConfiguration('namespace')),
-        
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_broadcaster_imu',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', '1',  
-                       PathJoinSubstitution(['imu']), 
-                       PathJoinSubstitution([LaunchConfiguration('namespace'), 'base_link/imu_sensor'])],
-            remappings = [
-                ('/tf', 'tf'), 
-                ('/tf_static', 'tf_static')
-            ]
-        ),
-        
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_broadcaster_lidar',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', '1', 
-                       PathJoinSubstitution(['lidar']), 
-                       PathJoinSubstitution([LaunchConfiguration('namespace'), 'base_link/rplidar'])],
-            remappings = [
-                ('/tf', 'tf'), 
-                ('/tf_static', 'tf_static')
-            ],
-        )
-    ])
 
+    # Workaround until Gazebo Ionic upgrades to sdformat 1.12 and allows <frame_id> tags
+    static_transforms_publisher = GroupAction(
+        [
+            PushRosNamespace(LaunchConfiguration("namespace")),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                name="static_transform_broadcaster_imu",
+                output="screen",
+                arguments=[
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "1",
+                    PathJoinSubstitution(["imu"]),
+                    PathJoinSubstitution(
+                        [LaunchConfiguration("namespace"), "base_link/imu_sensor"]
+                    ),
+                ],
+                remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+            ),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                name="static_transform_broadcaster_lidar",
+                output="screen",
+                arguments=[
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "1",
+                    PathJoinSubstitution(["lidar"]),
+                    PathJoinSubstitution(
+                        [LaunchConfiguration("namespace"), "base_link/rplidar"]
+                    ),
+                ],
+                remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+            ),
+        ]
+    )
 
     joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        namespace=LaunchConfiguration('namespace'),
-        output='screen',
-        parameters=[{'use_sim_time': LaunchConfiguration('run_in_simulation')}]
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        namespace=LaunchConfiguration("namespace"),
+        output="screen",
+        parameters=[{"use_sim_time": LaunchConfiguration("run_in_simulation")}],
     )
 
     # Define LaunchDescription variable
     ld = LaunchDescription(ARGUMENTS)
-    
+
     # Add nodes to LaunchDescription
     ld.add_action(robot_state_publisher)
     ld.add_action(static_transforms_publisher)
