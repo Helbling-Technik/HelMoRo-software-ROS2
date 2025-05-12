8d9bb7929c005c46037cb133c24ff9e196a5527c
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-12 07:33:12 +0000

    feat(description): apply black-formatter

--- src/helmoro_description/test/launch_description.test.py
+++ src/helmoro_description/test/launch_description.test.py
@@ -1,11 +1,11 @@
 import time
 import unittest
+
+
 import pytest
 import rclpy
 import std_msgs.msg
-import sensor_msgs.msg
 
-import launch_testing.markers
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -15,11 +15,12 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 
 from launch_testing.actions import ReadyToTest
+import launch_testing.markers
 from launch_testing.io_handler import ActiveIoHandler
+from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile, ReliabilityPolicy
 
-from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+NAMESPACE = "robot_namespace"
 
-NAMESPACE = 'robot_namespace'
 
 # Generate the launch description for testing the robot_state_publisher
 @pytest.mark.launch_test
@@ -27,26 +28,26 @@ NAMESPACE = 'robot_namespace'
 def generate_test_description():
     """Generate a LaunchDescription for the test."""
     # Define the package and launch file path
-    pkg_helmoro_description = get_package_share_directory('helmoro_description')
-    description_launch = PathJoinSubstitution([pkg_helmoro_description, 'launch', 'description.launch.py'])
+    pkg_helmoro_description = get_package_share_directory("helmoro_description")
+    description_launch = PathJoinSubstitution(
+        [pkg_helmoro_description, "launch", "description.launch.py"]
+    )
 
-    # Include the launch description with arguments    
+    # Include the launch description with arguments
     description = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([description_launch]),
-        launch_arguments=[
-            ('run_in_simulation', 'true'),
-            ('namespace', NAMESPACE)
-        ]   
+        launch_arguments=[("run_in_simulation", "true"), ("namespace", NAMESPACE)],
     )
-    
+
     # Action to confirm when the test is ready
     ready_to_test = TimerAction(period=0.5, actions=[ReadyToTest()])
-    
-     # Create and return the complete launch description
+
+    # Create and return the complete launch description
     ld = LaunchDescription()
     ld.add_action(description)
     ld.add_action(ready_to_test)
     return ld
+
 
 class TestCollection(unittest.TestCase):
     """Test suite for checking the robot_state_publisher functionality."""
@@ -54,7 +55,7 @@ class TestCollection(unittest.TestCase):
     def setUp(self):
         """Initialize the ROS node before each test."""
         rclpy.init()
-        self.node = rclpy.create_node('test_node')
+        self.node = rclpy.create_node("test_node")
 
     def tearDown(self):
         """Shut down the ROS node after each test."""
@@ -64,66 +65,68 @@ class TestCollection(unittest.TestCase):
     def test_robot_state_publisher_node_start(self, proc_output: ActiveIoHandler):
         """Test if the robot_state_publisher node has started."""
         found = False
-        print('Waiting for node...')
+        print("Waiting for node...")
         start = time.time()
-        
+
         # Wait for the node to start up and become available
         while time.time() - start < 10.0 and not found:
-            found = 'robot_state_publisher' in self.node.get_node_names()
+            found = "robot_state_publisher" in self.node.get_node_names()
             time.sleep(0.1)
 
         # Assert that the node was found
-        assert found, 'Node not found!'
-        
+        assert found, "Node not found!"
+
     def test_robot_state_publisher_advertise_topic(self, proc_output: ActiveIoHandler):
         """Test if the robot_description topic is advertised by the node."""
         received = False
         print("Listening for topics...")
         start = time.time()
-        
+
         # Wait for the topic to be advertised by the robot_state_publisher node
         while time.time() - start < 10.0 and not received:
             # Check if the node is publishing messages
             topic_names = self.node.get_topic_names_and_types()
             for topic_name, types in topic_names:
-                if topic_name == '/' + NAMESPACE + '/robot_description':
+                if topic_name == "/" + NAMESPACE + "/robot_description":
                     received = True
                     break
             time.sleep(0.1)
-        
-        # Assert that the topic was advertised 
-        assert received, 'Topic not advertised!'
+
+        # Assert that the topic was advertised
+        assert received, "Topic not advertised!"
 
     def test_robot_state_publisher_publish_msgs(self, proc_output: ActiveIoHandler):
         """Test if messages are published to the correct topic."""
         msgs_rx = []  # List to store received messages
-        
-         # Set up QoS profile with 'transient local' durability settings
+
+        # Set up QoS profile with 'transient local' durability settings
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.RELIABLE,
             durability=DurabilityPolicy.TRANSIENT_LOCAL,
             history=HistoryPolicy.KEEP_LAST,
-            depth=10
+            depth=10,
         )
-        
+
         # Create a subscription to the robot_description topic
         sub = self.node.create_subscription(
             std_msgs.msg.String,
-            '/' + NAMESPACE + '/robot_description',
+            "/" + NAMESPACE + "/robot_description",
             lambda msg: msgs_rx.append(msg),
-            qos_profile
+            qos_profile,
         )
-        
+
         try:
             # Wait for messages to be received on the topic for up to 10 seconds
             end_time = time.time() + 10
             while time.time() < end_time and len(msgs_rx) == 0:
                 # Spin once to execute the subscriber callback
                 rclpy.spin_once(self.node, timeout_sec=1)
-                
-             # Assert that at least one message has been received
-            assert len(msgs_rx) > 0, "No messages received on the robot_description topic!"
-            
+
+            # Assert that at least one message has been received
+            assert (
+                len(msgs_rx) > 0
+            ), "No messages received on the robot_description topic!"
+
         finally:
             # Ensure that the subscription is destroyed after the test
             self.node.destroy_subscription(sub)
@@ -131,33 +134,32 @@ class TestCollection(unittest.TestCase):
     def test_joint_state_publisher_node_start(self, proc_output: ActiveIoHandler):
         """Test if the joint_state_publisher node has started."""
         found = False
-        print('Waiting for node...')
+        print("Waiting for node...")
         start = time.time()
-        
+
         # Wait for the node to start up and become available
         while time.time() - start < 10.0 and not found:
-            found = 'joint_state_publisher' in self.node.get_node_names()
+            found = "joint_state_publisher" in self.node.get_node_names()
             time.sleep(0.1)
 
         # Assert that the node was found
-        assert found, 'Node not found!'
-        
+        assert found, "Node not found!"
+
     def test_joint_state_publisher_advertise_topic(self, proc_output: ActiveIoHandler):
         """Test if the joint_state topic is advertised by the node."""
         received = False
         print("Listening for topics...")
         start = time.time()
-        
+
         # Wait for the topic to be advertised by the robot_state_publisher node
         while time.time() - start < 10.0 and not received:
             # Check if the node is publishing messages
             topic_names = self.node.get_topic_names_and_types()
             for topic_name, types in topic_names:
-                if topic_name == '/' + NAMESPACE + '/joint_states':
+                if topic_name == "/" + NAMESPACE + "/joint_states":
                     received = True
                     break
             time.sleep(0.1)
-        
-        # Assert that the topic was advertised 
-        assert received, 'Topic not advertised!'
-         
+
+        # Assert that the topic was advertised
+        assert received, "Topic not advertised!"
