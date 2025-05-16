87954029e05ffc65f6b457e0a2623b9845b450a0
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-05-16 14:02:15 +0000

    feat(cli): Added a command line interface for starting/stopping simulation and spawning/deleting of robots

---
+++ src/helmoro_cli/helmoro_cli/robot_manager.py
@@ -0,0 +1,63 @@
+import os
+import re
+
+
+class RobotManager:
+    def __init__(self):
+        self.robots = {}
+
+    def spawn_robot(self, name, x, y, z, yaw):
+        self.robots[name] = {"pos": (x, y, z), "yaw": yaw, "status": "idle"}
+        # Here you would trigger a ROS 2 launch or service call
+        spawn = f"sudo NAMESPACE={name} USE_SIM_TIME=True \
+            docker compose -p robot_{name} -f /home/ws/.devcontainer/docker-compose.yml up description --detach --wait"
+        print(f"Start core nodes of {name}")
+        os.system(spawn)
+
+    def delete_robot(self, name):
+        "Stops all containers spawned by the robot"
+        raw_output = (
+            os.popen(
+                f"sudo docker container ls --filter name={name} --format '{{{{.ID}}}} {{{{.Names}}}}'"
+            )
+            .read()
+            .strip()
+        )
+
+        if not raw_output:
+            print(f"No running containers found for robot: {name}")
+            return
+
+        containers = [line.strip().split() for line in raw_output.splitlines()]
+        for container_id, container_name in containers:
+            result = os.system(
+                f"sudo docker container stop {container_id} --timeout 1 > /dev/null"
+            )
+            if result == 0:
+                print(f"{container_name}  Stopped")
+
+    def list_robots(self):
+        "Print out all spawned robots"
+        robots = self.get_robot_names()
+        if not robots:
+            print("No robots spawned.")
+        for name in robots:
+            print(name)
+
+    def get_robot_names(self):
+        "Return the list of robot names"
+        # Run the Docker command and read the output
+        stream = os.popen(
+            'docker container ls --filter name=robot --format "{{.Names}}"'
+        )
+        output = stream.read()
+
+        # Extract robot names using regex
+        robot_names = set()
+        pattern = re.compile(r"^robot_([^-\s]+)-")
+        for line in output.strip().split("\n"):
+            match = pattern.match(line)
+            if match:
+                robot_names.add(match.group(1))
+
+        return sorted(robot_names)
---
+++ src/helmoro_cli/helmoro_cli/simulation_manager.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+class SimulationManager:
+    def __init__(self):
+        self.robots = {}
+        self.simulation_status = "stopped"
+        self.pkg_dir = get_package_share_directory("helmoro_cli")
+        self.world = "empty"
+
+    def spawn_robot(self, name, x, y, z, yaw):
+        # Trigger a ROS 2 launch command to spawn the robot
+        spawn = f"sudo NAMESPACE={name} X={x} Y={y} Z={z} YAW={yaw} \
+            docker compose -p sim_{name} -f /home/ws/.devcontainer/docker-compose.yml up spawn_robot"
+
+        ros_gz_bridge = f"sudo NAMESPACE={name} WORLD={self.world} \
+            docker compose -p robot_{name} -f /home/ws/.devcontainer/docker-compose.yml up gz_ros_bridge --detach --wait"
+
+        print(f"Spawn {name} in gazebo simulation.")
+        os.system(spawn)
+        print(f"Attach ros_gz_bridge to {name}")
+        os.system(ros_gz_bridge)
+
+    def delete_robot(self, name):
+        "Deletes the robot from the simulation"
+        os.system(
+            f"sudo WORLD:={self.world} ENTITY_NAME={name} \
+            docker compose -p sim_{name} -f /home/ws/.devcontainer/docker-compose.yml up despawn_robot --detach --wait"
+        )
+
+    def get_status(self):
+        return self.simulation_status
+
+    def start_simulation(self, world):
+        # Start the simulation environment
+        print("Starting simulation environment...")
+        simulation = f"sudo docker compose -f /home/ws/.devcontainer/docker-compose.yml up simulation --detach --wait"
+        print(f"Executing command: {simulation}")
+        os.system(simulation)
+        self.simulation_status = "running"
+
+    def stop_simulation(self):
+        # Stop the simulation environment
+        print("Stopping simulation environment...")
+        stop_simulation = f"sudo docker compose -f /home/ws/.devcontainer/docker-compose.yml down simulation --timeout 1"
+        print(f"Executing command: {stop_simulation}")
+        os.system(stop_simulation)
+        self.simulation_status = "stopped"
