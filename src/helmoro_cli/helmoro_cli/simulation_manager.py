59b3b16b4817b7cbedcf9febe5fc2fb00b8fa5ca
Marc Blöchlinger <mbloechli@student.ethz.ch>
2025-06-04 08:57:10 +0000

    refactor: move docker specific files into seperate folder

--- src/helmoro_cli/helmoro_cli/simulation_manager.py
+++ src/helmoro_cli/helmoro_cli/simulation_manager.py
@@ -12,10 +12,10 @@ class SimulationManager:
     def spawn_robot(self, name, x, y, z, yaw):
         # Trigger a ROS 2 launch command to spawn the robot
         spawn = f"sudo NAMESPACE={name} X={x} Y={y} Z={z} YAW={yaw} \
-            docker compose -p sim_{name} -f /home/ws/.devcontainer/docker-compose.yml up spawn_robot"
+            docker compose -p sim_{name} -f /home/ws/docker/docker-compose.yml up spawn_robot"
 
         ros_gz_bridge = f"sudo NAMESPACE={name} WORLD={self.world} \
-            docker compose -p robot_{name} -f /home/ws/.devcontainer/docker-compose.yml up gz_ros_bridge --detach --wait"
+            docker compose -p robot_{name} -f /home/ws/docker/docker-compose.yml up gz_ros_bridge --detach --wait"
 
         print(f"Spawn {name} in gazebo simulation.")
         os.system(spawn)
@@ -26,7 +26,7 @@ class SimulationManager:
         "Deletes the robot from the simulation"
         os.system(
             f"sudo WORLD:={self.world} ENTITY_NAME={name} \
-            docker compose -p sim_{name} -f /home/ws/.devcontainer/docker-compose.yml up despawn_robot --detach --wait"
+            docker compose -p sim_{name} -f /home/ws/docker/docker-compose.yml up despawn_robot --detach --wait"
         )
 
     def get_status(self):
@@ -35,7 +35,7 @@ class SimulationManager:
     def start_simulation(self, world):
         # Start the simulation environment
         print("Starting simulation environment...")
-        simulation = f"sudo docker compose -f /home/ws/.devcontainer/docker-compose.yml up simulation --detach --wait"
+        simulation = f"sudo docker compose -f /home/ws/docker/docker-compose.yml up simulation --detach --wait"
         print(f"Executing command: {simulation}")
         os.system(simulation)
         self.simulation_status = "running"
@@ -43,7 +43,7 @@ class SimulationManager:
     def stop_simulation(self):
         # Stop the simulation environment
         print("Stopping simulation environment...")
-        stop_simulation = f"sudo docker compose -f /home/ws/.devcontainer/docker-compose.yml down simulation --timeout 1"
+        stop_simulation = f"sudo docker compose -f /home/ws/docker/docker-compose.yml down simulation --timeout 1"
         print(f"Executing command: {stop_simulation}")
         os.system(stop_simulation)
         self.simulation_status = "stopped"
